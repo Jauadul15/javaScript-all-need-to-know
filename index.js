f8692a32c1e8201266e0ab713635f1a29eb9bf78
Jauadul15 <rashik.gmh@gmail.com>
2023-07-18 14:02:23 +0600

    one file added

---
+++ index.js
@@ -0,0 +1,52 @@
+var a="agedg"
+var b="b"
+
+console.log(a,b)
+
+const n=Number(56.77)
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(1/0)
+
+var awww=null
+var qwe
+console.log(awww)
+console.log(qwe)
+
+
+
+
+
+var t=50
+var v=60
+
+const r=t -= v
+console.log(r)
+
+
+console.log(Math.E)
+console.log(Math.PI)
+
+var z=4.578
+console.log(Math.floor(z))
+console.log(Math.ceil(z))
+console.log(Math.abs(z))
+console.log(Math.round(z))
+console.log("==========")
+
+
+console.log(Math.max(59.7,59.8,59.9))
+console.log(Math.min(354,4567,56858,69,809))
+console.log(Math.pow(2,4))
+console.log(Math.sqrt(64))
+
+
+console.log(Math.floor(Math.random()*200 +1))
+
+
+
+
+
+
+
+
